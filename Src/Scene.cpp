a8675382b5288cc1bb7be57592f92b53066a9b9c
優幸 秋月 <akizuki.0618@outlook.jp>
2019-04-24 11:37:35 +0900

    シーンの変更をコンソールに出力
    デバッグのために、シーンの状態が変化したとき、その内容をコンソールに出力する

--- Src/Scene.cpp
+++ Src/Scene.cpp
@@ -10,7 +10,7 @@
 	@param name シーン名
 */
 Scene::Scene(const char*name) :name(name) {
-
+	std::cout << "Scene コンストラクタ:" << name << "\n";
 }
 
 /*
@@ -18,6 +18,7 @@ Scene::Scene(const char*name) :name(name) {
 */
 Scene::~Scene() {
 	Finalize();
+	std::cout << "Scene デストラクタ:" << name << "\n";
 }
 
 /*
@@ -25,6 +26,7 @@ Scene::~Scene() {
 */
 void Scene::Play() {
 	isActive = true;
+	std::cout << "Scene Play:" << name << "\n";
 }
 
 /*
@@ -32,6 +34,7 @@ void Scene::Play() {
 */
 void Scene::Stop() {
 	isActive = false;
+	std::cout << "Scene Stop:" << name << "\n";
 }
 
 /*
@@ -39,6 +42,7 @@ void Scene::Stop() {
 */
 void Scene::Show() {
 	isVisible = true;
+	std::cout << "Scene Show:" << name << "\n";
 }
 
 /*
@@ -46,6 +50,7 @@ void Scene::Show() {
 */
 void Scene::Hide() {
 	isVisible = false;
+	std::cout << "Scene Hide:" << name << "\n";
 }
 
 /*
